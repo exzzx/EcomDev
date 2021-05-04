a4b39239aaba93e7f4e5333f282c260df640ac32
exzzx <g359340318@gmail.com>
2021-05-03 18:16:09 -0700

    update do mapper

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.dao.UserDOMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"org.example"})
 @RestController
+@MapperScan("org.example.dao")
 public class App
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @GetMapping("/")
     public String accessHome(){
         return "Hello world!";
